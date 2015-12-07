f361d376c5549310adf172893248cb43f954f7f9
gitj <glenn.caltech@gmail.com>
2015-12-07 10:29:51 -0500

    initial commit, basic stuff seems to be working amplitudes are a bit off, need to figure out all scaling factors

---
+++ fake_data.py
@@ -0,0 +1,33 @@
+__author__ = 'gjones'
+
+import numpy as np
+
+class FakeKIDData(object):
+    def __init__(self,num_samples=2**19,num_detectors=16,
+                 red_exponent=-1.0,red_amplitude=1e-15,
+                 shot_amplitude_scale=1e-18,amplifier_amplitude_scale=1e-19,
+                 rolloff_freq=2e3, sampling_rate = 256e6/2**14,seed=123):
+        self.num_samples = num_samples
+        self.num_detectors = num_detectors
+        self.red_exponent = red_exponent
+        self.red_amplitude = red_amplitude
+        self.shot_amplitude_scale = shot_amplitude_scale
+        self.amplifier_amplitude_scale = amplifier_amplitude_scale
+        self.rolloff_freq = rolloff_freq
+        self.sampling_rate = sampling_rate
+        np.random.seed(seed)
+        self.shot_amplitudes = (np.random.rand(num_detectors)+.1)*self.shot_amplitude_scale
+        self.amplifier_amplitudes = (np.random.rand(num_detectors)+.1)*self.amplifier_amplitude_scale
+        self.regenerate()
+    def regenerate(self):
+        nfft = self.num_samples*2+1
+        fft_freq = np.linspace(0,self.sampling_rate/2,nfft)
+        red_fft = nfft*np.sqrt(120*self.red_amplitude*2/self.sampling_rate)*fft_freq**self.red_exponent * np.exp(np.pi*2j*np.random.rand(nfft))
+        red_fft[0] = 0
+        self.red_process = np.fft.irfft(red_fft)[:self.num_samples]
+        shot_fft = np.random.randn(nfft,self.num_detectors)+1j*np.random.randn(nfft,self.num_detectors)
+        shot_fft = shot_fft*nfft*np.sqrt(120*self.shot_amplitudes*2/self.sampling_rate)
+        amplifier_fft = np.random.randn(nfft,self.num_detectors)+1j*np.random.randn(nfft,self.num_detectors)
+        amplifier_fft = amplifier_fft*nfft*np.sqrt(120*self.amplifier_amplitudes*2/self.sampling_rate)
+        self.total_signal = np.fft.irfft((red_fft[:,None]*np.sqrt(self.shot_amplitudes/self.shot_amplitudes.max())+shot_fft)/((1+1j*fft_freq/(self.rolloff_freq))[:,None])+amplifier_fft,axis=0)[:self.num_samples,:]
+        self.signal_without_red_noise = np.fft.irfft((shot_fft)/((1+1j*fft_freq/(self.rolloff_freq))[:,None])+amplifier_fft,axis=0)[:self.num_samples,:]
---
+++ svd_model.py
@@ -0,0 +1,17 @@
+__author__ = 'gjones'
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+class SVDNoiseModel(object):
+    def __init__(self,data,sampling_rate=256e6/2**14):
+        self.data = data
+        self.sampling_rate = sampling_rate
+    def construct_model(self,n_components=1,nfft=2**15):
+        data_fft = np.fft.rfft(self.data,axis=0)
+        U,S,V = np.linalg.svd(data_fft[:nfft,:],full_matrices=False)
+        common_mode_fft = np.dot(np.dot(U[:,:n_components],np.diag(S[:n_components])),np.conj(V[:n_components,:]))
+        full_common_mode_fft = np.empty_like(data_fft)
+        full_common_mode_fft[:nfft,:] = common_mode_fft
+        self.common_mode = np.fft.irfft(full_common_mode_fft,axis=0)
+        self.corrected_data = self.data-self.common_mode
